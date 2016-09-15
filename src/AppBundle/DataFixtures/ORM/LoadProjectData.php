960ebe7a6df0dcf95c75f070ddd7bd15561a2fed
DIJOUX Vincent <dijoux.vin@gmail.com>
2016-09-15 17:49:48 +0200

    add description in Project entity

--- src/AppBundle/DataFixtures/ORM/LoadProjectData.php
+++ src/AppBundle/DataFixtures/ORM/LoadProjectData.php
@@ -24,13 +24,17 @@ class LoadProjectData implements FixtureInterface
     public function load(ObjectManager $manager)
     {
         $project = new Project();
-        $project->setName('Test');
-
+        $project
+            ->setName('Test')
+            ->setDescription('Ceci est un test')
+        ;
         $manager->persist($project);
 
         $project = new Project();
-        $project->setName('Symfony');
-
+        $project
+            ->setName('Symfony')
+            ->setDescription('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi consequuntur doloremque excepturi ipsa magnam minus nulla, odio vel veniam voluptate. Ab amet asperiores doloremque id mollitia nesciunt quia quod repudiandae!')
+        ;
         $manager->persist($project);
 
         $manager->flush();
