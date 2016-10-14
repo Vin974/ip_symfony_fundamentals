1a8ad788d135e56f62fd1d56ae5230281acadfad
DIJOUX Vincent <dijoux.vin@gmail.com>
2016-10-14 11:43:53 +0200

    add fos userbundle

---
+++ src/UserBundle/Entity/User.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vin
+ * Date: 14/10/2016
+ * Time: 11:26
+ */
+
+namespace UserBundle\Entity;
+
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\UserBundle\Model\User as BaseUser;
+
+/**
+ * Class User
+ * @package AppBundle\Entity
+ *
+ * @ORM\Entity()
+ * @ORM\Table(name="kanban_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Id()
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="fullname", type="string")
+     */
+    protected $fullname;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getFullname(): string
+    {
+        return $this->fullname;
+    }
+
+    /**
+     * @param string $fullname
+     * @return User
+     */
+    public function setFullname(string $fullname): User
+    {
+        $this->fullname = $fullname;
+        return $this;
+    }
+}
